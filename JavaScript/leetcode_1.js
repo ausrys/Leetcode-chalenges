a648a00f6bf89bff9133c221b6c2fa4070e48248
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-04-16 16:26:35 +0300

    js challenges

---
+++ JavaScript/leetcode_1.js
@@ -0,0 +1,29 @@
+
+/* 
+
+Given an array of integers nums and an integer target, return indices of the
+two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may
+not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+function twoSum(nums, target) {
+    let num_map = {}
+    for (let index = 0; index <nums.length; index++){
+        let complement = target - nums[index];
+        if (Object.hasOwn(num_map, complement)) return [num_map[complement], index]
+        num_map[nums[index]] = index
+    }
+    return []
+}
+
+console.log((twoSum([3,3], 6)))
--- JavaScript/leetcode_3498.js
+++ JavaScript/leetcode_3498.js
@@ -11,10 +11,9 @@
 function reverse_degree(string) {
     let result = 0;
     for (let i in string) {
-        // result += Math.abs((string.charCodeAt(i)- 123) * (i+1));
-        console.log(typeof(i))
+        result += Math.abs((string.charCodeAt(i)- 123) * (parseInt(i)+1));
     };
     return result;
 }
 
-console.log(reverse_degree("za"))
+console.log(reverse_degree("zaza")) // 160
