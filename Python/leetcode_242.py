c67d4aabbbc73f28bf0a27792fae9927661cddf6
ausrys <ausryszz1@gmail.com>
2025-05-02 08:36:34 +0300

    39 and 242

---
+++ Python/leetcode_242.py
@@ -0,0 +1,15 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+'''
+
+def isAnagram( s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26  # Assuming only lowercase letters
+
+        for ch1, ch2 in zip(s, t):
+            count[ord(ch1) - ord('a')] += 1
+            count[ord(ch2) - ord('a')] -= 1
+
+        return all(c == 0 for c in count)
---
+++ Python/leetcode_39.py
@@ -0,0 +1,31 @@
+'''
+Given an array of distinct integers candidates and a target integer target,
+return a list of all unique combinations of candidates where the chosen numbers
+sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times.
+Two combinations are unique if the frequency of at least one of the chosen
+numbers is different.
+
+The test cases are generated such that the number of unique
+combinations that sum up to target is less than 150 combinations
+for the given input.
+'''
+
+def combination_sum(candidates, target):
+    result = []
+
+    def backtrack(start, current_combination, total):
+        if total == target:
+            result.append(list(current_combination))
+            return
+        if total > target:
+            return
+
+        for i in range(start, len(candidates)):
+            current_combination.append(candidates[i])
+            backtrack(i, current_combination, total + candidates[i])  # allow reuse of same number
+            current_combination.pop()  # backtrack
+
+    backtrack(0, [], 0)
+    return result
