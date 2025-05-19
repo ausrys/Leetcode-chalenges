3840a608d03105e1245cab044cc706cada9696c3
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-05-19 14:19:01 +0300

    12 js

---
+++ JavaScript/leetcode_12.js
@@ -0,0 +1,18 @@
+var intToRoman = function(num) {
+    const valMap = [
+        [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+        [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+        [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+    ];
+
+    let roman = '';
+    for (let [value, symbol] of valMap) {
+        const count = Math.floor(num / value);
+        num %= value;
+        roman += symbol.repeat(count);
+    }
+
+    return roman;
+};
+
+console.log((intToRoman(345)));
